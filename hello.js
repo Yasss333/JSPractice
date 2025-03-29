6a491f2351cf452939004affeb97ca49bda8237b
Yasss333 <yashmandhare3@gmail.com>
2025-03-29 14:38:40 +0530

    learning gitand github

---
+++ hello.js
@@ -0,0 +1,1 @@
+console.log("hello chaicode.comvds apna colleg e");
---
+++ part1/print.js
@@ -0,0 +1,5 @@
+console.log("hello yash ");
+console.log("mandhare  ");
+process.stdout.write("chai code");
+console.table({ city:"panchgani "});
+console.warn({city:"jaipur});
